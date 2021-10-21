0997916dd36e48f6a7888fa7cfc7fd67ce059bbb
Murtaza <mtz.rashmy@gmail.com>
2021-10-21 16:13:56 +0530

    First Commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void PostJira() {
+		// TODO Auto-generated method stub
+		System.out.println("PostJira");
+	}
+
+	
+	
+	@Test
+	public void deleteTwitter() {
+		// TODO Auto-generated method stub
+		System.out.println("deleteTwitter");
+	}
+	
+
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,18 @@
+package qaclickacademy;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+
+	
+	@Test
+	public void BrowserAutomation() {
+		// TODO Auto-generated method stub
+		System.out.println("BrowserAutomation");
+	}
+	
+	@Test
+	public void ElementsUI() {
+		// TODO Auto-generated method stub
+		System.out.println("ElementsUI");
+	}
+}
